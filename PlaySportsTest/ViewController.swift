9fda8d7cf067e7603ae896bc30325df68ad8aac2
Ravikanth Marri <ravikanthm@rs-macbook-pro.local>
2019-02-14 22:46:14 +0000

    Initial Commit

---
+++ PlaySportsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaySportsTest
+//
+//  Created by R K Marri on 14/02/2019.
+//  Copyright © 2019 R K Marri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
